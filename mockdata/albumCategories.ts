fd188fd6436fb281fb32542663f496e806a310ca
allezz <aerbay@gmail.com>
2020-09-28 18:54:17 +0200

    Add mock data for album categories

---
+++ mockdata/albumCategories.ts
@@ -0,0 +1,106 @@
+const albumCategories = [
+  {
+    id: "1",
+    title: "Jazz Vibes",
+    albums: [
+      {
+        id: "1",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Stan Getz, Cannonball Adderley",
+      },
+      {
+        id: "2",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Charlie Parker, Gerry Mulligan",
+      },
+      {
+        id: "3",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "John Coltrane, Scott Hamilton",
+      },
+      {
+        id: "4",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Miles Davis, Oliver Nelson",
+      },
+    ],
+  },
+  {
+    id: "2",
+    title: "Rock Vibes",
+    albums: [
+      {
+        id: "4",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Audioslave, Travis",
+      },
+      {
+        id: "5",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Radiohead, Muse",
+      },
+      {
+        id: "6",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Coldplay, Weezer",
+      },
+      {
+        id: "7",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Soundgarden, Stone Temple Pilots",
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "Rock Vibes",
+    albums: [
+      {
+        id: "8",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Audioslave, Travis",
+      },
+      {
+        id: "9",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Radiohead, Muse",
+      },
+      {
+        id: "10",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Coldplay, Weezer",
+      },
+      {
+        id: "11",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Soundgarden, Stone Temple Pilots",
+      },
+    ],
+  },
+  {
+    id: "4",
+    title: "Rock Vibes",
+    albums: [
+      {
+        id: "12",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Audioslave, Travis",
+      },
+      {
+        id: "13",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Radiohead, Muse",
+      },
+      {
+        id: "14",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Coldplay, Weezer",
+      },
+      {
+        id: "15",
+        imageUri: "https://picsum.photos/200",
+        artistHeadline: "Soundgarden, Stone Temple Pilots",
+      },
+    ],
+  },
+];
