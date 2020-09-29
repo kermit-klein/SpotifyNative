81ac9f479f1601cfe07199450e8b1f982ce38ceb
allezz <aerbay@gmail.com>
2020-09-29 11:33:40 +0200

    Add song type, Add song list item component

---
+++ components/SongListItem/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, Image, View } from "react-native";
+
+import styles from "./styles";
+import { Song } from "../../types";
+
+export type SongListItemProps = {
+  song: Song;
+};
+
+const SongListItem = (props: SongListItemProps) => {
+  const { song } = props;
+
+  return (
+    <View>
+      <Image source={{ uri: song.imageUri }} style={styles.image} />
+      <Text style={styles.title}>{song.title}</Text>
+      <Text style={styles.artist}>{song.artist}</Text>
+    </View>
+  );
+};
+
+export default SongListItem;
