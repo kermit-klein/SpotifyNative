369200b014ccc31bfdfd829e21a8a177db744734
allezz <aerbay@gmail.com>
2020-09-29 11:09:13 +0200

    Add album mock data on AlbumScreen

--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -2,6 +2,41 @@ import { useRoute } from "@react-navigation/native";
 import React, { useEffect } from "react";
 import { View, Text } from "react-native";
 
+const album = {
+  id: "11",
+  name: "Good Vibes",
+  by: "Spotify",
+  numberOfLikes: 345,
+  imageUri: "https://picsum.photos/200",
+  artistHeadline: "Taylor Swift, Justin Timberlake",
+  songs: [
+    {
+      id: "1",
+      imageUri: "https://picsum.photos/200",
+      title: "High on You",
+      artist: "Helen",
+    },
+    {
+      id: "2",
+      imageUri: "https://picsum.photos/200",
+      title: "Lorem Ipsum",
+      artist: "Lorem",
+    },
+    {
+      id: "3",
+      imageUri: "https://picsum.photos/200",
+      title: "Creep",
+      artist: "Radiohead",
+    },
+    {
+      id: "4",
+      imageUri: "https://picsum.photos/200",
+      title: "High on You",
+      artist: "Helen",
+    },
+  ],
+};
+
 const AlbumScreen = () => {
   const route = useRoute();
 
