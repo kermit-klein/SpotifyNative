a4092296ad06261412851d89d39fdf7b8daa6057
allezz <aerbay@gmail.com>
2020-09-29 22:57:01 +0200

    Style player widget

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, Image, View } from "react-native";
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
 
 import styles from "./styles";
 
@@ -15,8 +16,14 @@ const PlayerWidget = () => {
     <View style={styles.container}>
       <Image source={{ uri: song.imageUri }} style={styles.image} />
       <View style={styles.rightContainer}>
-        <Text style={styles.title}>{song.title}</Text>
-        <Text style={styles.artist}>{song.artist}</Text>
+        <View style={styles.nameContainer}>
+          <Text style={styles.title}>{song.title}</Text>
+          <Text style={styles.artist}>{song.artist}</Text>
+        </View>
+        <View style={styles.iconContainer}>
+          <AntDesign name="hearto" size={30} color={"white"} />
+          <FontAwesome name="play" size={30} color={"white"} />
+        </View>
       </View>
     </View>
   );
--- components/PlayerWidget/styles.ts
+++ components/PlayerWidget/styles.ts
@@ -4,26 +4,43 @@ const styles = StyleSheet.create({
   container: {
     position: "absolute",
     flexDirection: "row",
-    margin: 15,
-    bottom: 34,
+    bottom: 49,
     width: "100%",
-    backgroundColor: "black",
+    backgroundColor: "#131313",
+    borderWidth: 2,
+    borderColor: "black",
+    alignItems: "center",
   },
   rightContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flex: 1,
+  },
+  nameContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  iconContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: 100,
     justifyContent: "space-around",
-    marginLeft: 10,
   },
   title: {
     color: "white",
-    fontSize: 20,
+    fontSize: 18,
+    fontWeight: "bold",
+    fontFamily: "sans-serif",
+    margin: 10,
   },
   artist: {
     color: "lightgrey",
-    fontSize: 16,
+    fontSize: 18,
   },
   image: {
     width: 75,
     height: 75,
+    marginRight: 10,
   },
 });
 
