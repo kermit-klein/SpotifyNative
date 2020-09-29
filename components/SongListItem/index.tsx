1cc64883ecd29d59c82b22cb4b07e67e53b0c7ec
allezz <aerbay@gmail.com>
2020-09-29 12:17:55 +0200

    Display an album song, add styling to look better

--- components/SongListItem/index.tsx
+++ components/SongListItem/index.tsx
@@ -12,10 +12,12 @@ const SongListItem = (props: SongListItemProps) => {
   const { song } = props;
 
   return (
-    <View>
+    <View style={styles.container}>
       <Image source={{ uri: song.imageUri }} style={styles.image} />
-      <Text style={styles.title}>{song.title}</Text>
-      <Text style={styles.artist}>{song.artist}</Text>
+      <View style={styles.rightContainer}>
+        <Text style={styles.title}>{song.title}</Text>
+        <Text style={styles.artist}>{song.artist}</Text>
+      </View>
     </View>
   );
 };
--- components/SongListItem/styles.ts
+++ components/SongListItem/styles.ts
@@ -1,11 +1,21 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    margin: 15,
+  },
+  rightContainer: {
+    justifyContent: "space-around",
+    marginLeft: 10,
+  },
   title: {
     color: "white",
+    fontSize: 20,
   },
   artist: {
     color: "lightgrey",
+    fontSize: 16,
   },
   image: {
     width: 75,
