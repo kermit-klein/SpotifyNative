e1bd48397322f169d65174f800ffa94c93800a99
allezz <aerbay@gmail.com>
2020-10-01 01:47:18 +0200

    Play my first song on native

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, Image, View } from "react-native";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { Audio } from "expo-av";
 
 import styles from "./styles";
 
 const song = {
   id: "1",
+  uri:
+    "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_2MG.mp3",
   imageUri: "https://picsum.photos/200",
   title: "High on You",
   artist: "Helen",
 };
 
 const PlayerWidget = () => {
+  const onPlaybackStatusUpdate = (status) => {
+    console.log(status);
+  };
+
+  const playCurrentSong = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      { uri: song.uri },
+      { shouldPlay: true },
+      onPlaybackStatusUpdate
+    );
+  };
+
+  useEffect(() => {
+    playCurrentSong();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={{ uri: song.imageUri }} style={styles.image} />
